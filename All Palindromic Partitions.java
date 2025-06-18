901d2158cadb9e1172ab520803ac9d1bc48a3781
Preeti788 <kumaripreetispj@gmail.com>
2025-06-18 17:36:33 +0530

    All Palindromic Partitions.java

---
+++ All Palindromic Partitions.java
@@ -0,0 +1,35 @@
+class Solution {
+    public ArrayList<ArrayList<String>> palinParts(String s) {
+        // code here
+        
+        ArrayList<ArrayList<String>> result = new ArrayList<>();
+        ArrayList<String> current = new ArrayList<>();
+        backtrack(0, s, current, result);
+        return result;
+    }
+
+    private void backtrack(int start, String s, ArrayList<String> current, ArrayList<ArrayList<String>> result) {
+        if (start == s.length()) {
+            result.add(new ArrayList<>(current));
+            return;
+        }
+
+        for (int end = start + 1; end <= s.length(); end++) {
+            String substr = s.substring(start, end);
+            if (isPalindrome(substr)) {
+                current.add(substr);
+                backtrack(end, s, current, result);
+                current.remove(current.size() - 1);
+            }
+        }
+    }
+
+    private boolean isPalindrome(String str) {
+        int left = 0, right = str.length() - 1;
+        while (left < right) {
+            if (str.charAt(left++) != str.charAt(right--))
+                return false;
+        }
+        return true;
+    }
+}
